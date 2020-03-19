519ec5a9b88da04bb4330a8b620e54a410f01e86
loginashok <60283219+loginashok@users.noreply.github.com>
2020-03-19 12:27:36 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import './App.css';
-import Navigation from './component/Navigation';
-import Slider from './component/Slider';
+import './css/App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import First from './First';
+import Frontpage from './Frontpage';
+import Contact from './Contact';
+import Gallery from './Gallery';
+import About from './About';
+import { BrowserRouter as Router, Switch, Route, BrowserRouter,} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      <Slider />
+    <div className="App"> 
+    <BrowserRouter>
+      <Switch>
+      <Route exact path="/" component={Frontpage} />
+      <Route exact path="/First" component={First} />
+      <Route exact path="/Gallery" component={Gallery} />
+      <Route exact path="/About" component={About} />
+      <Route exact path="/Contact" component={Contact} />
+      </Switch>
+      </BrowserRouter>
       
-    </div>
+      
+      
+      </div>
+      
+   
   );
 }
 
